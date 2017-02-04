1667dc6cadb0f0c991f9ffb9a1ae327cceb2667a
Antoinemd <antoine.mnd@gmail.com>
2017-02-04 02:16:03 +0100

    update unsetAllCookies

--- TP3/exercices/unsetAllCookies.php
+++ TP3/exercices/unsetAllCookies.php
@@ -1,15 +1,26 @@
 <?php 
 
-if (isset($_COOKIE['cookie_bgColor']) || isset($_COOKIE['cookie_bgColor'])) {
+// if (isset($_COOKIE['cookie_bgColor']) || isset($_COOKIE['cookie_bgColor'])) {
 	
-	unset($_COOKIE['cookie_bgColor']);
-	unset($_COOKIE['cookie_textColor']);
+// 	unset($_COOKIE['cookie_bgColor']);
+// 	unset($_COOKIE['cookie_textColor']);
 
-	if (empty($_COOKIE['cookie_bgColor']) && empty($_COOKIE['cookie_textColor'])) {
-		echo "cookie_bgColor and cookie_textColor deleted ! <br> ";
-	}
+// 	if (empty($_COOKIE['cookie_bgColor']) && empty($_COOKIE['cookie_textColor'])) {
+// 	}
+// }
+
+
+if (isset($_SERVER['HTTP_COOKIE'])) {
+    $cookies = explode(';', $_SERVER['HTTP_COOKIE']);
+    foreach($cookies as $cookie) {
+        $parts = explode('=', $cookie);
+        $name = trim($parts[0]);
+        setcookie($name, '', time()-1000);
+        setcookie($name, '', time()-1000, '/');
+    }
 }
 
+echo "All cookies has been deleted ! <br> ";
 echo "<a href='exo5.couleurs.php'> Retour </a> ";
 
 ?>
